c7885abafb17978690c40a43c45436b2250dcece
Abhinav Reddy <abhinavrdy3@gmail.com>
2023-09-23 18:46:28 +0530

    I did my first command.

--- credentials.py
+++ credentials.py
@@ -2,3 +2,5 @@ import mysql.connector as mysql
 
 def defaultDatabase() -> mysql.MySQLConnection:
     return mysql.connect(host="localhost", user="root", password="mysql123", database="pyproj")
+    pass
+
